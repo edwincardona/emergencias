c15492ae52ef1572d28c559525a8376800c69a49
edwincardona <cardonaedwin@hotmail.com>
2013-11-13 16:54:53 -0500

    se agrega la conexión a mongolab
    Esta libreria mogodb permite alamcenar los datos en una DB en la nube
    NoSQL

--- js/controller.js
+++ js/controller.js
@@ -2,10 +2,18 @@
 
 /* Controllers */
 
-var emergenciasApp = angular.module('emergenciasApp', []);
+var emergenciasApp = angular.module('emergenciasApp', ['mongolabResource']);
 
-emergenciasApp.controller('EmergenciasListCtrl', function EmergenciasListCtrl($scope) {
-  $scope.emergencias = [
+emergenciasApp.constant('API_KEY', '1CrbVfw7cBn4lsZHdu4REj0mY6lW7dbp');
+emergenciasApp.constant('DB_NAME', 'emergencias');
+
+emergenciasApp.factory('Evento', function($mongolabResource){
+  return $mongolabResource('eventos');
+});
+
+emergenciasApp.controller('EventosListCtrl', function EventosListCtrl($scope, $http, Evento) {
+/*
+  $scope.eventos = [
     {'tipo': 'Incendio',
      'descripcion': 'Quema de bosques.',
     },
@@ -16,8 +24,32 @@ emergenciasApp.controller('EmergenciasListCtrl', function EmergenciasListCtrl($s
      'descripcion': 'En el río.',
     }
   ];
+ */
+  $scope.eventos = Evento.query({_id:''});
 
   $scope.guardarEmergencia = function(tipo, descripcion){
-    $scope.emergencias.push({'tipo':tipo, 'descripcion':descripcion});
+//    $scope.emergencias.push({'tipo':tipo, 'descripcion':descripcion});
+    $.ajax({ url:'https://api.mongolab.com/api/1/databases/emergencias/collections/eventos/'+
+        '?apiKey=1CrbVfw7cBn4lsZHdu4REj0mY6lW7dbp',
+        data: JSON.stringify({'tipo':tipo, 'descripcion':descripcion}),
+        type:"POST",
+        contentType: "application/json"
+      }).success(function (data, status, header, config){
+        console.log("ingreso");
+      }).error( function (data, status, headers, config) {
+        console.log("pailas");
+      });
+
   }
+
+  $scope.getEventos = function (){
+    $http.get('https://api.mongolab.com/api/1/databases/emergencias/collections/eventos/'+
+      '?apiKey=1CrbVfw7cBn4lsZHdu4REj0mY6lW7dbp'
+    ).success(function(data,status){
+      console.log(data);
+    }).error(function(data, status){
+      console.log(status);
+    });
+  }
+
 });
